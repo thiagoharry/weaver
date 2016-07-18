042f19939d37bf8288a2dc4faa33ed613699baae
Thiago "Harry" Leucz Astrizi <harry@bitbitbit.com.br>
2016-07-18 09:05:47 -0300

    Generated new base file.

--- project/basefile.c
+++ project/basefile.c
@@ -1,8 +1,17 @@
 #include "game.h"
 
-int main(void){
-  Winit();
+void main_loop(void){ // The game loop
+  if(W.keyboard[W_ANY])
+    Wexit(); // If the user presses any key, quit (you should change this)
 
-  Wexit();
+  // (...) Put the loop logic here
+
+  Wrest(10); // Spends 14 milisseconds idle (70fps)
+}
+
+int main(void){
+  Winit(); // Initializes Weaver
+  Wloop(main_loop); // Enter a new game loop
+  Wexit(); // Quit the program
   return 0;
 }
