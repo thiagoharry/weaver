c65fb02a8a5fff61adf2dbe4ecfe5e11aedbd6e1
Thiago "Harry" Leucz Astrizi <harry@bitbitbit.com.br>
2016-08-10 10:03:24 -0300

    Corrigido arquivo base para ficar de acordo com definição atual de Wloop.

--- project/basefile.c
+++ project/basefile.c
@@ -1,12 +1,11 @@
 #include "game.h"
 
-void main_loop(void){ // The game loop
+MAIN_LOOP main_loop(void){ // The game loop
  BEGIN_LOOP_INITIALIZATION:
   // Put here initialization code to be executed just in the first iteration 
  END_LOOP_INITIALIZATION:
   if(W.keyboard[W_ANY])
-    Wexit(); // If the user presses any key, quit (you should change this)
-
+    Wexit_loop(); // If the user presses any key, quit (you should change this)
   // (...) Put the loop logic here
 
   Wrest(10); // Spends 14 milisseconds idle (70fps)
