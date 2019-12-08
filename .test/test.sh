9a23e3f147e1dc219d91c07930e7206df0c70569
Thiago 'Harry' Leucz Astrizi <harry@bitbitbit.com.br>
2019-12-08 17:25:25 -0300

    Primeiro teste passando no Windows

--- .test/test.sh
+++ .test/test.sh
@@ -20,14 +20,36 @@ function assertEqual(){
     fi
 }
 
+function assertEqualFiles(){
+    LENGTH_STRING=${#1}
+    NUMBER_OF_TESTS=$((${NUMBER_OF_TESTS} + 1))
+    echo -n ${1}
+    for (( i=2; i <= $((65-${LENGTH_STRING})); ++i )); do
+	echo -n "."
+    done
+    if  cmp -s "${2}" "${3}" > /dev/null ; then
+	    echo "[OK]"
+	    OK=$((${OK}+1))
+    else
+	    echo "[FAIL]"
+	    FAIL=$((${FAIL}+1))
+    fi
+}
+
 function print_result(){
     echo ${NUMBER_OF_TESTS}" tests: "${OK}" sucess, "${FAIL}" fails"
     echo
 }
 
 function test_invocation(){
-    OUTPUT=$(./.test/bin/weaver)
-    TEXT="    .  .     You are outside a Weaver Directory.
+    if [ "${OSTYPE}" == "msys" ]; then
+        ./.test/bin/weaver > .test/file1.dat
+        echo -ne "    .  .     You are outside a Weaver Directory.\r\n   .|  |.    The following command uses are available:\r\n   ||  ||\r\n   \\\\\\()//  weaver\r\n   .={}=.      Print this message and exits.\r\n  / /\`'\\ \\\\\r\n  \` \\  / '  weaver PROJECT_NAME\r\n     \`'        Creates a new Weaver Directory with a new\r\n               project.\r\n" > .test/file2.dat
+        assertEqualFiles "Testing simple invocation" .test/file1.dat .test/file2.dat
+        rm .test/file1.dat .test/file2.dat
+    else
+        OUTPUT=$(./.test/bin/weaver)
+        TEXT="    .  .     You are outside a Weaver Directory.
    .|  |.    The following command uses are available:
    ||  ||
    \\\\()//  weaver
@@ -36,8 +58,12 @@ function test_invocation(){
   \` \\  / '  weaver PROJECT_NAME
      \`'        Creates a new Weaver Directory with a new
                project."
-    assertEqual "Testing simple invocation" "${OUTPUT}" "${TEXT}"
+
+        assertEqual "Testing simple invocation" "${OUTPUT}" "${TEXT}"
+    fi
 }
+
+echo -e "Running tests...\n"
 
 test_invocation
 print_result
